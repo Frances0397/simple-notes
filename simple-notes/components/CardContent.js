d96e69c4c498f1411e5cc3b328fd5a68e851f209
Frances0397 <kayleigh0385@gmail.com>
2023-08-05 18:04:40 +0200

    implemented note content editing for new and old notes

--- simple-notes/components/CardContent.js
+++ simple-notes/components/CardContent.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Dimensions, TextInput } from 'react-native';
 import { Card, Input, Button } from '@rneui/themed';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
 const screenWidth = Dimensions.get('window').width;
@@ -12,32 +12,83 @@ const cardWidth = (screenWidth * cardWidthPercentage) / 100;
 const marginValue = 15; // Adjust this value to set the desired margin  
 
 export default function CardContent() {
+    var newNote = true; //I check wheter I'm editing an existing note or creating a new one by retrieving the id from the navigation
+
+    const navigation = useNavigation();
+    const route = useRoute();
+    const noteId = route.params?.id;
+
+    const [data, setData] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    if (noteId !== '-1') {
+        newNote = false;
+    }
+
+    useEffect(() => {
+        if (noteId !== '-1') {
+            fetchData(noteId);
+            console.log("use effect event");
+            console.log(data === "");
+            console.log(loading);
+        } else {
+            setLoading(false); // Set loading to false when no noteId is provided
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log("use effect 2");
+        console.log(data);
+        if (data !== "") {
+            setLoading(false);// When data has been loaded then stop showing loader
+        }
+    }, [data]);
+
+    const fetchData = async (sId) => {
+        try {
+            const response = await axios.get('http://192.168.1.62:3000/note/' + sId);
+            console.log("fetch data");
+            console.log(response.data);
+            let jsonString = response.data;
+            // const jsonData = JSON.parse(jsonString);
+            setData(response.data); // Store the fetched data in the state
+            // setLoading(false);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            // setLoading(false);
+        }
+    };
+
     const showInfo = () => {
         alert("show note info");
     };
 
-    return (
-        <ScrollView>
-            <Card
-                //  key={item.id} 
-                containerStyle={styles.cardContainer} wrapperStyle={styles.card}
-            >
-                {/* <Card.Title style={styles.title}>Contenuto</Card.Title> */}
-                <TextInput numberOfLines={1} ellipsizeMode="tail" style={styles.title}>Titolo</TextInput>
-                <Card.Divider />
-                {/* <Text style={styles.text}></Text> */}
-                {/* <Input inputStyle={styles.text}>Lorem er ultricies, lacus lectus gravida s</Input> */}
-                <TextInput multiline numberOfLines={null} style={styles.text}>rat dictum, magna sem porttitor elit</TextInput>
-                <View style={styles.bottomContainer}>
-                    <Button
-                        icon={<Ionicons name="information-circle-outline" size={24} color="#FFECD1" />}
-                        buttonStyle={styles.infoButton}
-                        onPress={showInfo}
-                    />
-                </View>
-            </Card>
-        </ScrollView>
-    );
+    if (loading) {
+        return <Text style={styles.text}>Loading...</Text>;
+    } else {
+        return (
+            <ScrollView>
+                <Card
+                    //  key={item.id} 
+                    containerStyle={styles.cardContainer} wrapperStyle={styles.card}
+                >
+                    {/* <Card.Title style={styles.title}>Contenuto</Card.Title> */}
+                    <TextInput numberOfLines={1} ellipsizeMode="tail" style={styles.title} placeholder='Title'>{newNote ? "" : data.title}</TextInput>
+                    <Card.Divider />
+                    {/* <Text style={styles.text}></Text> */}
+                    {/* <Input inputStyle={styles.text}>Lorem er ultricies, lacus lectus gravida s</Input> */}
+                    <TextInput multiline numberOfLines={null} style={styles.text} placeholder='Content'>{newNote ? "" : data.content}</TextInput>
+                    <View style={styles.bottomContainer}>
+                        <Button
+                            icon={<Ionicons name="information-circle-outline" size={24} color="#FFECD1" />}
+                            buttonStyle={styles.infoButton}
+                            onPress={showInfo}
+                        />
+                    </View>
+                </Card>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
